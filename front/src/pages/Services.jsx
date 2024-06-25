3e8e9f6d21d8dc60e09816a774c8ee015e94b2c3
Victoria Cavanna <viccavanna@gmail.com>
2024-06-25 13:02:15 -0300

    Inicio Admin

--- front/src/pages/Services.jsx
+++ front/src/pages/Services.jsx
@@ -1,42 +1,13 @@
-import { Container, Col, Row } from "react-bootstrap"
-import { useState, useEffect } from "react";
-import axios from "axios";
-import AddForm from "../components/AddForm"
-import Table from '../components/TableAppoinment';
-import '../assets/css/HeaderStyles.css'
-import '../assets/css/ServicesStyles.css'
+
+import ServicesSection from './../components/ServicesSection';
 
 const Services = () => {
-  const [services, setServices] = useState([]);
-
-    const fetchServices = async () => {
-        try {
-            const response = await axios.get('http://localhost:3000/api/services',  { credentials:"include" });
-            setServices(response.data);
-        } catch (error) {
-            console.error('Error fetching services:', error);
-        }
-    };
-
-    useEffect(() => {
-        fetchServices();
-    }, []);
+  
   return (
     <>
-      <Container>
-        <section className="services-section d-flex flex-column">
-          <Row>
-            <h2 className="text-center subtitle">Agregar Servicio</h2>
-            <AddForm />
-          </Row>
-        </section>
-        <section className="services-section d-flex flex-column">
-          <Row >
-            <h2 className="text-center subtitle">Lista de turnos</h2>
-            <Table services={services}/>
-          </Row>
-        </section>
-      </Container>
+    <section className='services-section d-flex'>
+      <ServicesSection/>
+    </section>
     </>
   )
 }
