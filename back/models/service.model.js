8fe62442ca36f1402f78b488af622cafdc30fea6
jiturre <66223727+jiturre@users.noreply.github.com>
2024-12-04 12:04:25 -0300

    Format code

--- back/models/service.model.js
+++ back/models/service.model.js
@@ -2,29 +2,29 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const serviceSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    duration: {
-        type: Number, 
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export default mongoose.model('Service', serviceSchema);
+export default mongoose.model("Service", serviceSchema);
